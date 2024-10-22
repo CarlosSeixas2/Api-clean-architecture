9f2ccee006583fcae9fd61f76ac1a8280c1cb9e4
Seixas <carlosseixasof@gmail.com>
2024-10-22 09:14:29 -0300

    Refactoring env config module

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -11,6 +11,6 @@ export class EnvConfigService implements EnvConfig {
   }
 
   getNodeEnv(): string {
-    return this.configService.get<string>('NodeEnv')
+    return this.configService.get<string>('NODE_ENV')
   }
 }
